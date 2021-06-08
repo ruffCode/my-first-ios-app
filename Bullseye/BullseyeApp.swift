45a1992b00a2a65593f9cb73c27e6bfba24d57b5
Alexi Bre <alexi@ruffCode.com>
2021-06-07 20:02:26 -0400

    part 2 - state

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Alexi Bre on 6/7/21.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bullseye/ContentView.swift
@@ -0,0 +1,61 @@
+//
+//  ContentView.swift
+//  Bullseye
+//
+//  Created by Alexi Bre on 6/7/21.
+//
+
+import SwiftUI
+
+
+struct ContentView: View {
+
+
+    @State
+    private var alertIsVisible = false
+
+    var body: some View {
+        VStack {
+            Text("🎯🎯🎯\nPUT THE BULLSEYE AS YOU CAN TO")
+                    .bold()
+                    .kerning(2.0)
+                    .multilineTextAlignment(.center)
+                    .lineSpacing(4.0)
+                    .font(.footnote)
+
+            Text("89")
+                    .kerning(-1.0)
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+
+            HStack {
+                Text("1").bold().font(.headline).padding()
+                Slider(value: .constant(50.0), in:
+                0.1...100.0).accentColor(.red)
+                Text("100").bold().font(.headline)
+                        .padding()
+
+            }
+            Button(action: {
+                self.alertIsVisible = true
+            }) {
+                Text("Hit me")
+            }.alert(isPresented: $alertIsVisible, content: {
+                Alert(title: Text("Hola!"), message: Text("You're been alerted!"),
+                        dismissButton: .default(Text("Cool beans, bro")))
+            })
+        }
+
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            ContentView()
+            ContentView()
+        }
+        ContentView()
+                .previewLayout(.fixed(width: 658, height: 320))
+    }
+}
